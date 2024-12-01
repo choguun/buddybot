4152b57e8951f7f162f37a6a575c2f2fae0f0800
choguun <visaruth.s@gmail.com>
2024-12-01 12:23:06 +0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -79,11 +79,11 @@ def status():
     })
 
 @app.route('/instructions', methods=['GET'])
-def setup_status():
+def instructions():
     return jsonify({
         "status": "OK",
         "message": "Enable and enjoy! Just ask your questions and I'll do my best to answer them."
-})
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT, debug=DEBUG)
